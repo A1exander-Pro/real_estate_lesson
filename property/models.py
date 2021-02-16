05f4c69a4fff933dfdde654533ce25a19df66794
A1exander-Pro <76167473+A1exander-Pro@users.noreply.github.com>
2021-02-16 18:39:03 +0300

    Update models.py

--- property/models.py
+++ property/models.py
@@ -6,19 +6,24 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
-    owner_pure_phone = PhoneNumberField(blank=True, verbose_name="Нормализованный номер владельца")
+    owner_pure_phone = PhoneNumberField(blank=True,
+                                        verbose_name="Нормализованный номер владельца")
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление",
                                       default=timezone.now, db_index=True)
     new_building = models.NullBooleanField()
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
-    town = models.CharField("Город, где находится квартира", max_length=50, db_index=True)
-    town_district = models.CharField("Район города, где находится квартира", max_length=50,
+    town = models.CharField("Город, где находится квартира",
+                            max_length=50, db_index=True)
+    town_district = models.CharField("Район города, где находится квартира",
+                                     max_length=50,
                                      blank=True,
                                      help_text='Чертаново Южное')
-    address = models.TextField("Адрес квартиры", help_text='ул. Подольских курсантов д.5 кв.4')
-    floor = models.CharField("Этаж", max_length=3, help_text='Первый этаж, последний этаж, пятый этаж')
+    address = models.TextField("Адрес квартиры",
+                               help_text='ул. Подольских курсантов д.5 кв.4')
+    floor = models.CharField("Этаж", max_length=3,
+                             help_text='Первый этаж, последний этаж, пятый этаж')
     rooms_number = models.IntegerField("Количество комнат в квартире", db_index=True)
     living_area = models.IntegerField("количество жилых кв.метров", null=True,
                                       blank=True, db_index=True)
@@ -33,8 +38,10 @@ class Flat(models.Model):
 
 
 class Complaint(models.Model):
-    name = models.ForeignKey(User, verbose_name="Кто жаловался", on_delete=models.CASCADE)
-    flat = models.ForeignKey(Flat, verbose_name="Квартира на которую жаловались", related_name="complaints",
+    name = models.ForeignKey(User, verbose_name="Кто жаловался",
+                             on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat, verbose_name="Квартира на которую жаловались",
+                             related_name="complaints",
                              on_delete=models.CASCADE)
     complaint_text = models.TextField(verbose_name="Текст жалобы")
 
@@ -46,8 +53,10 @@ class Owner(models.Model):
     full_name = models.CharField(max_length=200, verbose_name="ФИО Владельца",
                                  db_index=True)
     phone_number = models.CharField("Номер владельца", max_length=20)
-    pure_phone_number = PhoneNumberField(blank=True, verbose_name="Нормализованный номер владельца")
-    owned_apartments = models.ManyToManyField(Flat, verbose_name="Квартиры в собственности",
+    pure_phone_number = PhoneNumberField(blank=True,
+                                         verbose_name="Нормализованный номер владельца")
+    owned_apartments = models.ManyToManyField(Flat,
+                                              verbose_name="Квартиры в собственности",
                                               related_name="owners", db_index=True)
 
     def __str__(self):
